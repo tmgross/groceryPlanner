f60e24a7f48277b7b06e14c22bb844e7f874927b
tmgross <tmgross8@gmail.com>
2023-11-07 16:52:43 -0500

    Added additional styling options for boxes

--- boxStyles.js
+++ boxStyles.js
@@ -10,6 +10,13 @@ export const box_styles = StyleSheet.create({
     marginVertical: 10,
     borderRadius: 10,
   },
+  bigBox: {
+    width: '90%',
+    flex: 1,
+    backgroundColor: '#dcdcdc',
+    marginVertical: 10,
+    borderRadius: 10,
+  },
   boxText: {
     fontFamily: "'Trebuchet MS', Helvetica, sans-serif", //FIX THIS SO IT USES THE FONT
     fontSize: 18,
@@ -31,6 +38,18 @@ export const box_styles = StyleSheet.create({
     top: 10,
     right: 10,
     width: 30,
+    alignItems: 'center',
+    borderRadius: 10,
+    borderColor: 'black',
+    borderWidth: 1,
+    backgroundColor: 'black',
+  },
+  lowerBoxButton: {
+    position: 'absolute',
+    bottom: 10,
+    right: 10,
+    padding: 10,
+    width: 'auto',
     alignItems: 'center',
     borderRadius: 10,
     borderColor: 'black',
