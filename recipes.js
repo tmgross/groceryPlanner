84407f9fc4ddf2301237ac0c87f3abdb53167fe7
tmgross <tmgross8@gmail.com>
2023-11-10 16:50:09 -0500

    Updated styling for recipe modal, created a modal for create new recipe, allows for multiple ingredient addition

--- recipes.js
+++ recipes.js
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { ScrollView, View, Text, TouchableOpacity } from 'react-native';
+import {
+  ScrollView,
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
 import { styles } from './styles';
 import { box_styles } from './boxStyles';
 import { modal_styles } from './modalStyles';
@@ -12,21 +18,51 @@ function RecipeScreen() {
   const navigation = useNavigation();
   const [recipes, setRecipes] = useState([]);
   const [numRecipes, setNumRecipes] = useState(0);
+
   const [isModalVisible, setModalVisible] = useState(false);
   const [selectedRecipe, setSelectedRecipe] = useState(null);
+  const [isCreateModalVisible, setCreateModalVisible] = useState(false);
 
-  const openModal = recipe => {
+  const [recipeName, setRecipeName] = useState('');
+  const [ingredients, setIngredients] = useState([]);
+  const [ingredientInput, setIngredientInput] = useState('');
+
+  const openRecipeModal = recipe => {
     setSelectedRecipe(recipe);
     setModalVisible(true);
   };
 
-  const closeModal = () => {
+  const closeRecipeModal = () => {
     setSelectedRecipe(null);
     setModalVisible(false);
   };
 
+  const handleCreateNew = () => {
+  };
+
+  const openCreateModal = () => {
+    setCreateModalVisible(true);
+  };
+
+  const closeCreateModal = () => {
+    setCreateModalVisible(false);
+  };
+
   const handleLogout = () => {
     navigation.navigate('Home');
+  };
+
+  const addIngredient = () => {
+    if (ingredientInput !== '') {
+      setIngredients([...ingredients, ingredientInput]);
+      setIngredientInput('');
+    }
+  };
+
+  const removeIngredient = index => {
+    const newIngredients = [...ingredients];
+    newIngredients.splice(index, 1);
+    setIngredients(newIngredients);
   };
 
   useEffect(() => {
@@ -52,8 +88,6 @@ function RecipeScreen() {
     };
   }, []);
 
-  const handleCreateNew = () => {};
-
   return (
     <ScrollView contentContainerStyle={styles.homePageScrn}>
       <TouchableOpacity onPress={handleLogout} style={styles.logoutBtn}>
@@ -66,14 +100,14 @@ function RecipeScreen() {
           <Text style={box_styles.boxHeader}>Recipe List</Text>
         </View>
         <TouchableOpacity
-          onPress={() => handleCreateNew()}
+          onPress={() => openCreateModal()}
           style={box_styles.lowerBoxButton}>
           <Text style={box_styles.boxButtonText}>Create New Recipe</Text>
         </TouchableOpacity>
         <View style={box_styles.separator}></View>
         {recipes.map((recipe, index) => (
           <View key={recipe.id}>
-            <TouchableOpacity onPress={() => openModal(recipe)}>
+            <TouchableOpacity onPress={() => openRecipeModal(recipe)}>
               <Text style={box_styles.boxListItem}>{recipe.Description}</Text>
             </TouchableOpacity>
             {index < recipes.length && (
@@ -83,22 +117,9 @@ function RecipeScreen() {
         ))}
       </View>
 
-      {/* {recipes.map((recipe) => (
-  <View key={recipe.id} style={box_styles.recipeItemContainer}>
-    <TouchableOpacity onPress={() => openModal(recipe)}>
-      <View style={box_styles.recipeItemContent}>
-        {recipe.image && (
-          <Image source={{ uri: recipe.image }} style={box_styles.recipeImage} />
-        )}
-        <Text style={box_styles.boxListItem}>{recipe.Description}</Text>
-      </View>
-    </TouchableOpacity>
-  </View>
-))} */}
-
       <Modal
         isVisible={isModalVisible}
-        onBackdropPress={closeModal}
+        onBackdropPress={closeRecipeModal}
         animationIn="slideInUp"
         animationOut="slideOutDown">
         <View style={modal_styles.modalContainer}>
@@ -109,20 +130,81 @@ function RecipeScreen() {
               </Text>
               <View>
                 <Text style={styles.modalTitle}>Ingredients</Text>
-                <Text>
-                  {selectedRecipe.ingredients.map((ingredient, index) => (
-                    <Text key={index}>
-                      {`\u2022 ${ingredient}`}
-                      {'\n'}
-                    </Text>
-                  ))}
-                </Text>
+                {selectedRecipe.ingredients &&
+                selectedRecipe.ingredients.length > 0 ? (
+                  <Text>
+                    {selectedRecipe.ingredients.map((ingredient, index) => (
+                      <Text key={index}>
+                        {`\u2022 ${ingredient}`}
+                        {'\n'}
+                      </Text>
+                    ))}
+                  </Text>
+                ) : (
+                  <Text>No ingredients provided</Text>
+                )}
               </View>
               {/* Add more recipe information here */}
             </View>
           )}
           <TouchableOpacity
-            onPress={closeModal}
+            onPress={closeRecipeModal}
+            style={modal_styles.closeModalButton}>
+            <Text style={modal_styles.closeModalButtonText}>Close</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
+
+      {/* Modal for creating a new recipe */}
+      <Modal
+        isVisible={isCreateModalVisible}
+        onBackdropPress={closeCreateModal}
+        animationIn="slideInUp"
+        animationOut="slideOutDown">
+        <View style={modal_styles.modalContainer}>
+          <ScrollView>
+            <Text style={modal_styles.modalTitle}>Create New Recipe</Text>
+            <View>
+              <Text style={modal_styles.modalHeader}>Name:</Text>
+              <TextInput
+                style={modal_styles.input}
+                value={recipeName}
+                onChangeText={text => setRecipeName(text)}
+                placeholder="Enter recipe name"
+              />
+            </View>
+
+            <View>
+              <Text style={modal_styles.modalHeader}>Ingredients:</Text>
+              {ingredients.map((ingredient, index) => (
+                <View key={index} style={modal_styles.ingredientsContainer}>
+                  <Text>{`${index + 1}. ${ingredient}`}</Text>
+                  <TouchableOpacity onPress={() => removeIngredient(index)}>
+                    <Text style={modal_styles.removeIngredient}>Remove Ingredient</Text>
+                  </TouchableOpacity>
+                </View>
+              ))}
+              <View style={modal_styles.addIngredientContainer}>
+                <TextInput
+                  style={modal_styles.input}
+                  value={ingredientInput}
+                  onChangeText={text => setIngredientInput(text)}
+                  placeholder="Enter ingredient"
+                />
+                <TouchableOpacity onPress={addIngredient}>
+                  <Text style={modal_styles.addIngredient}>Add Ingredient</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </ScrollView>
+          <TouchableOpacity
+            onPress={handleCreateNew}
+            style={modal_styles.acceptModalButton}>
+            <Text style={modal_styles.acceptModalButtonText}>Create</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={closeCreateModal}
             style={modal_styles.closeModalButton}>
             <Text style={modal_styles.closeModalButtonText}>Close</Text>
           </TouchableOpacity>
